2f5cc803a113a5293e417d0dfdc58d01e4aa51ea
NACHIKETA <NACHIKETA@DESKTOP-9VGV2VS>
2023-12-31 11:58:14 +0530

    Commiting 'Reverse_String' to BranchX.

---
+++ src/gitDemo2023dec/ReverseString.java
@@ -0,0 +1,10 @@
+package gitDemo2023dec;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+StringBuffer sb=new StringBuffer("Nachiketa Mohapatra");
+System.out.println(sb.reverse());
+	}
+
+}
